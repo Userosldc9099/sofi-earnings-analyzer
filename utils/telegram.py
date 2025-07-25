247af6cdbe1a19b71780ba2fcdb1a5aa79d0013f
Userosldc9099 <tr4deboo@gmail.com>
2025-07-25 19:46:58 +0700

    Update telegram.py

---
+++ utils/telegram.py
@@ -0,0 +1,9 @@
+import os
+import requests
+
+def send_telegram(message: str):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message}
+    requests.post(url, data=payload)
